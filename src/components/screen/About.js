897619d40d548b8411ac8031d6d2829ad5bf65f2
rohinidev-bit <rohiniverma53@gmail.com>
2021-07-19 00:32:49 +0530

    fixed a bug

--- src/components/screen/About.js
+++ src/components/screen/About.js
@@ -2,76 +2,85 @@ import React from 'react';
 import './about.css'
 const Picture = () => {
 	return (
-		<>
-			<div
-				style={{}}
-			><div class="wrap">
-					<div class="about">
-						<div class="about-intro">
-							<h4>About</h4>
-							<h1>Let me introduce myself</h1>
-							<div class="intro-details">
-								<div class="intro-image">
-									<div class="image">
-										<img class="image-circle" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAARNAAAAJDg2OGM4NzA4LTk2ODgtNDJhYy1iOWNhLWUwNjk2NDg3ODdmZA.jpg" alt=""></img>
-									</div>
+		<div>
+			<div class="wrap">
+				<div class="about">
+					<div class="about-intro">
+						<h4>About</h4>
+						<div class="intro-details">
+							<div class="intro-image">
+								<div class="image">
+									<img class="image-circle" src="https://nurserynisarga.in/wp-content/uploads/2019/10/Red-Rose.jpg" alt=""></img>
 								</div>
-								<div class="intro-paragraph padding"><p>Lorem ipsum Exercitation culpa qui dolor consequat exercitation fugiat laborum ex ea eiusmod ad do aliqua occaecat nisi ad irure sunt id pariatur Duis laboris amet exercitation veniam labore consectetur ea id quis eiusmod.</p></div>
 							</div>
+							<div class="intro-paragraph padding"> <p>I love developing apps and websites, petting my dog and reading philosphy.  </p></div>
 						</div>
-						<div class="about-info">
-							<div class="profile padding">
-								<h2>Profile</h2>
-								<p>Lorem ipsum Qui veniam ut consequat ex ullamco nulla in non ut esse in magna sint minim officia consectetur nisi commodo ea magna pariatur nisi cillum.</p>
-								<h3>Fullname:</h3>
-								<p>Ieuan Tomos Walker</p>
-								<h3>Job:</h3>
-								<p>Student</p>
-							</div>
-							<div class="skills padding">
-								<h2>Skills</h2>
-								<p>Lorem ipsum Qui veniam ut consequat ex ullamco nulla in non ut esse in magna sint minim officia consectetur nisi commodo ea magna pariatur nisi cillum.</p>
-								<ul class="skill-bars">
-									<li>
-										<div class="progress percent90"></div>
-										<strong>HTML5</strong>
-									</li>
-									<li>
-										<div class="progress percent85"></div>
-										<strong>CSS3</strong>
-									</li>
-									<li>
-										<div class="progress percent70"></div>
-										<strong>JQuery</strong>
-									</li>
-									<li>
-										<div class="progress percent95"></div>
-										<strong>PHP</strong>
-									</li>
-									<li>
-
-										<div class="progress percent75"></div>
-										<strong>Wordpress</strong>
-									</li>
-
-								</ul>
-
-
-							</div>
+					</div>
+					<div class="about-info">
+						<div class="profile padding">
+							<h2>Profile</h2>
+							<p>I love</p>
+							<h3>Fullname:</h3>
+							<p>Miss Rohini Verma</p>
+							<p>Full Stack Developer</p>
 						</div>
-						<div class="about-buttons padding">
-							<div class="hire-me">
-								<button class="about-button">Hire me</button>
-							</div>
-							<div class="download-cv">
-								<button class="about-button">Download CV</button>
-							</div>
+						<div class="skills padding">
+							<h2>Skills</h2>
+							<p> NUlll </p>
+							<ul class="skill-bars">
+								<li>
+									<div class="progress percent95"></div>
+									<strong>JavaScript</strong>
+								</li>
+								<li>
+									<div class="progress percent95"></div>
+									<strong>React/React Native</strong>
+								</li>
+								<li>
+									<div class="progress percent95"></div>
+									<strong>Node</strong>
+								</li>
+								<li>
+									<div class="progress percent90"></div>
+									<strong>HTML5</strong>
+								</li>
+								<li>
+									<div class="progress percent85"></div>
+									<strong>CSS</strong>
+								</li>
+								<li>
+									<div class="progress percent70"></div>
+									<strong>DBMS</strong>
+								</li>
+								<li>
+									<div class="progress percent70"></div>
+									<strong>C++</strong>
+								</li>
+								<li>
+									<div class="progress percent70"></div>
+									<strong>Python</strong>
+								</li>
+								<li>
+									<div class="progress percent70"></div>
+									<strong></strong>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="about-buttons padding">
+						<div class="hire-me">
+							<button class="about-button"><a href="https://mail.google.com/mail/?view=cm&fs=1&to=rohiniverma53@gmail.com">Discuss a Project !</a>
+							</button>
+						</div>
+						<div class="download-cv">
+							<button class="about-button"><a href="/images/Resume.pdf" download="Rohini's Resume" target="_blank"> Download Resume</a></button>
 						</div>
 					</div>
 				</div>
-
 			</div>
-		</>
+
+		</div>
+
 	)
 }
 
--- src/components/screen/WorkEx.js
+++ src/components/screen/WorkEx.js
@@ -1,27 +1,40 @@
 import "./work.css"
+import SC from './images/sc.jpg'
+import GP from './images/GP.jpg'
 const WorkEX = (props) => {
 	return (
-		<div style={{
-			padding: "14rem",
-		}}>
-			<ul class="timeline">
-				<div style={{ display: 'inline-block', paddingLeft: ' 4rem' }}>
-					<li data-text="08/2016 12/2016" data-year="Echo Magazine"></li>
-				</div>
-				<div style={{ display: 'inline-block', paddingLeft: '14rem' }}>
-					<li data-text="06/2019 07/2019 " data-year="Adept"></li>
-				</div>
-				<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
-					<li data-text="07/2020 08/2020" data-year="Find Mind"></li>
-				</div>
-				<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
-					<li data-text="09/2020 11/2020" data-year="Digital Duet"></li>
-				</div>
-				<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
-					<li data-text="06/2021 Present" data-year="Star Catalysts"></li>
-				</div>
+		<div>
+			<div style={{
+				padding: "14rem",
+			}}>
+				<ul class="timeline">
+					<div style={{ display: 'inline-block', paddingLeft: ' 4rem' }}>
+						<li data-text="08/2016 12/2016" data-year="Echo Magazine"></li>
+					</div>
+					<div style={{ display: 'inline-block', paddingLeft: '14rem' }}>
+						<li data-text="06/2019 07/2019 " data-year="Adept"></li>
+					</div>
+					<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
+						<li data-text="07/2020 08/2020" data-year="Find Mind"></li>
+					</div>
+					<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
+						<li data-text="09/2020 11/2020" data-year="Digital Duet"></li>
+					</div>
+					<div style={{ display: 'inline-block', paddingLeft: ' 14rem' }}>
+						<li data-text="06/2021 Present" data-year="Star Catalysts"></li>
+					</div>
 
-			</ul>
+				</ul>
+			</div>
+			<div>
+				<p style={{ color: "#222222", "font-family": "Lato, sans-serif", "font-size": "25px", "font-weight": "300", "line-height": "58px", "margin": "0 0 58px" }}> Currently I am working on </p>
+				<div className="row">
+					<img src={SC} alt="" style={{ height: "20vh", width: "20vh" }} />
+					<div class="plus"></div>
+					<img src={GP} alt="" style={{ height: "20vh", width: "20vh" }} />
+
+				</div>
+			</div>
 		</div>
 
 	);
